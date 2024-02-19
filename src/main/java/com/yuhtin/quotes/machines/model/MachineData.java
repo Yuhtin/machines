e394a6587a17d2e96d716ed07ba83850923e6c57
Davi Duarte <rio.daviduarte@gmail.com>
2024-02-19 15:44:01 -0300

    feat: paste schematic & new data model

--- src/main/java/com/yuhtin/quotes/machines/model/MachineData.java
+++ src/main/java/com/yuhtin/quotes/machines/model/MachineData.java
@@ -1,18 +1,42 @@
 package com.yuhtin.quotes.machines.model;
 
+import com.yuhtin.quotes.machines.schematic.Schematic;
+import com.yuhtin.quotes.machines.util.ItemBuilder;
 import lombok.Builder;
 import lombok.Getter;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Builder
 public class MachineData {
 
     private final int id;
-    private final String customName, schematicFileName;
+    private final ItemStack item;
+    private final Schematic schematic;
+    private final String customName;
+    private final List<String> lore;
     private final int requiredFuelId, spendFuelInterval;
-    private final double price, sellDropPrice;
+    private final double price, sellDropPrice, maxDrops;
 
-    private final ItemStack drop;
+    public void cleanUpSchematic(Location location, double yawPlaced) {
+        schematic.cleanUpSchematic(location, yawPlaced);
+    }
 
+    public boolean placeSchematic(Player player, Location decode, Runnable completed) {
+        Collection<Location> locations = schematic.pasteSchematic(decode, player, completed, Schematic.Options.REALISTIC, Schematic.Options.IGNORE_TRANSPARENT);
+        return locations != null && !locations.isEmpty();
+    }
+
+    public ItemStack generateItem() {
+        ItemBuilder itemBuilder = new ItemBuilder(item);
+        itemBuilder.setNBTInt("machine-data", id);
+        itemBuilder.setNBTString("machine-unique", String.valueOf(System.currentTimeMillis()));
+
+        return itemBuilder.wrap();
+    }
 }
