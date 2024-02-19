1b18509d1454a7b78460c7ba350a7809ee16849d
Davi Duarte <rio.daviduarte@gmail.com>
2024-02-19 15:44:08 -0300

    feat: new logic

--- src/main/java/com/yuhtin/quotes/machines/model/Machine.java
+++ src/main/java/com/yuhtin/quotes/machines/model/Machine.java
@@ -1,52 +1,79 @@
 package com.yuhtin.quotes.machines.model;
 
+import com.yuhtin.quotes.machines.cache.MachineCache;
+import com.yuhtin.quotes.machines.cache.MachineDataCache;
 import com.yuhtin.quotes.machines.util.EncodedLocation;
 import lombok.Builder;
 import lombok.Data;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 
 @Data
 @Builder
 public class Machine {
 
-    private final String owner;
+    private final String owner, customName;
     private final int encodedLocation;
+    private final double yawPlaced, maxDrops;
     private final int machineDataId, fuelConsumeInterval;
 
-    private boolean active;
+    private boolean buildingSchematic, active;
 
     private int cycles, fuelAmount;
 
-    private double quantity, drops;
+    private double drops;
 
-    public boolean tick() {
-        if (!active) return false;
+    public void tick() {
+        if (!active) return;
 
         if (cycles < 0) {
             cycles = fuelConsumeInterval;
             fuelAmount--;
         } else {
             cycles--;
-            drops += quantity;
+            drops++;
+
+            if (drops > maxDrops) {
+                desactivate();
+            }
         }
 
         if (fuelAmount < 0) {
-            active = false;
-            cycles = -1;
+            desactivate();
             fuelAmount = 0;
         }
-
-        return active;
     }
 
     public void fuelUp(int amount) {
         fuelAmount += amount;
+        activate();
     }
 
     public void activate() {
         active = true;
+    }
+
+    private void desactivate() {
+        active = false;
+        cycles = -1;
+    }
+
+    public boolean pasteSchematic(Player player, World world) {
+        MachineDataCache cache = MachineDataCache.instance();
+        MachineData data = cache.get(machineDataId);
+        if (data == null) return false;
+
+        buildingSchematic = true;
+        return data.placeSchematic(player, EncodedLocation.of(encodedLocation).decode(world), () -> buildingSchematic = false);
+    }
+
+    public void cleanUpSchematic(World world) {
+        MachineDataCache cache = MachineDataCache.instance();
+        MachineData data = cache.get(machineDataId);
+        if (data == null) return;
+
+        data.cleanUpSchematic(EncodedLocation.of(encodedLocation).decode(world), yawPlaced);
     }
 
     @Override
