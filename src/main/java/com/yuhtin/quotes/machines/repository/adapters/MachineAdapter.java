25124835395d6851df9787075e6dcaf9097d632c
Davi Duarte <rio.daviduarte@gmail.com>
2024-02-19 15:43:41 -0300

    feat: machine sql adapter

--- src/main/java/com/yuhtin/quotes/machines/repository/adapters/MachineAdapter.java
+++ src/main/java/com/yuhtin/quotes/machines/repository/adapters/MachineAdapter.java
@@ -2,19 +2,27 @@ package com.yuhtin.quotes.machines.repository.adapters;
 
 import com.henryfabio.sqlprovider.executor.adapter.SQLResultAdapter;
 import com.henryfabio.sqlprovider.executor.result.SimpleResultSet;
+import com.yuhtin.quotes.machines.cache.MachineDataCache;
 import com.yuhtin.quotes.machines.model.Machine;
+import com.yuhtin.quotes.machines.model.MachineData;
 
 public class MachineAdapter implements SQLResultAdapter<Machine> {
 
     @Override
     public Machine adaptResult(SimpleResultSet resultSet) {
         boolean active = resultSet.get("active");
+        int machineDataId = resultSet.get("machine_data_id");
+
+        MachineDataCache cache = MachineDataCache.instance();
+        MachineData data = cache.get(machineDataId);
 
         return Machine.builder()
                 .owner(resultSet.get("username"))
+                .customName(data.getCustomName())
                 .encodedLocation(resultSet.get("encodedLocation"))
-                .machineDataId(resultSet.get("machine_data_id"))
-                .fuelConsumeInterval(resultSet.get("fuel_consume_interval"))
+                .machineDataId(machineDataId)
+                .fuelConsumeInterval(data.getSpendFuelInterval())
+                .maxDrops(data.getMaxDrops())
                 .active(active)
                 .cycles(resultSet.get("cycles"))
                 .fuelAmount(resultSet.get("fuel_amount"))
